8cfa021ab155040b8481b777901cb22cf82a7fc1
handlerone451 <aldinkaisar451@gmail.com>
2024-12-04 08:17:52 +0700

    fix

--- requirements.bat
+++ requirements.bat
@@ -3,4 +3,4 @@
 rem Install npm packages
 echo Installing npm packages...
 timeout /t 3 > nul
-start cmd.exe /c "npm install && npm run build && winget install Gyan.FFmpeg && pip install -r requirements.txt && copy .env.example .env"
+start cmd.exe /c "npm install && npm run build && winget install Gyan.FFmpeg && pip install -r requirements.txt && copy env.example .env"
